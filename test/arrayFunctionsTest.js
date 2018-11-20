b7cc4630bf77598cbf4228740ce7b5d58ed80584
Tilak Puli <34330361+brain-eater@users.noreply.github.com>
2018-11-20 14:57:02 +0530

    Added new cases for testing map

--- test/arrayFunctionsTest.js
+++ test/arrayFunctionsTest.js
@@ -1,6 +1,6 @@
 const lib = require("../src/arrayFunctionsLib.js");
 const map = lib.map;
-const assert = require("assert");
+const assert = require("assert").deepEqual;
 
 const square = function(number){
   return number*number;
@@ -13,4 +13,14 @@ const testMap = function(func,input,expectedOutput){
 }
 
 testMap(square,[],[]);
+testMap(square,[2],[4]);
+testMap(square,[1,2,3],[1,4,9]);
 
+console.log("Checking whether input is changing or not ");
+let numbers = [1,2,3,4,5];
+let squares = map(square,numbers);
+assert(numbers,[1,2,3,4,5]);
+
+console.log("checking whether it returning same array reference");
+map(square,[1,2]);
+assert(squares,[1,4,9,16,25]);
