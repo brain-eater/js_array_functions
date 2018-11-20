0c43152c5f55b3f4edc4f291b400410fe7777f67
Tilak Puli <34330361+brain-eater@users.noreply.github.com>
2018-11-20 18:48:17 +0530

    wrote test using mocha

--- test/arrayFunctionsTest.js
+++ test/arrayFunctionsTest.js
@@ -1,26 +1,27 @@
 const lib = require("../src/arrayFunctionsLib.js");
 const map = lib.map;
-const assert = require("assert").deepEqual;
+const deepEqual = require("assert").deepEqual;
 
 const square = function(number){
   return number*number;
 }
 
-const testMap = function(func,input,expectedOutput){
-  let actualOutput = map(func,input);
-  console.log("Testing with",input);
-  assert(actualOutput,expectedOutput);
+const stringLength = function(text){
+  return text.length;
 }
 
-testMap(square,[],[]);
-testMap(square,[2],[4]);
-testMap(square,[1,2,3],[1,4,9]);
+describe('map',function(){
+ //Testing map for square function
+ it('should return squares of array values',function() {
+   deepEqual(map(square,[]),[]);
+   deepEqual(map(square,[1]),[1]);
+   deepEqual(map(square,[1,2]),[1,4]);
+ });
 
-console.log("Checking whether input is changing or not ");
-let numbers = [1,2,3,4,5];
-let squares = map(square,numbers);
-assert(numbers,[1,2,3,4,5]);
-
-console.log("checking whether it returning same array reference");
-map(square,[1,2]);
-assert(squares,[1,4,9,16,25]);
+ //Testing map for inputs as string
+ it('should find the length of strings in an array',function() {
+   deepEqual(map(stringLength,[]),[]);
+   deepEqual(map(stringLength,["computer"]),[8]);
+   deepEqual(map(stringLength,["computer","abcd"]),[8,4]);
+ });
+});
