d1497102bc91a16768e1fcb619db874f670f6bd6
Tilak Puli <34330361+brain-eater@users.noreply.github.com>
2018-11-21 16:11:24 +0530

    Added new test cases

--- test/arrayFunctionsTest.js
+++ test/arrayFunctionsTest.js
@@ -23,45 +23,66 @@ const add = function(num1,num2){
 
 
 describe('map',function(){
-  //Testing map for square function
-  it('should return squares of array values',function() {
+  it('should return empty array when empty array is given as input',function() {
     deepEqual(map(square,[]),[]);
-    deepEqual(map(square,[1]),[1]);
-    deepEqual(map(square,[1,2]),[1,4]);
   });
 
-  //Testing map for inputs as string
-  it('should find the length of strings in an array',function() {
-    deepEqual(map(stringLength,[]),[]);
-    deepEqual(map(stringLength,["computer"]),[8]);
-    deepEqual(map(stringLength,["computer","abcd"]),[8,4]);
+  it('should work if only one element is given',function() {
+    deepEqual(map(square,[2]),[4]);
+  });
+
+  it('should work with more than one elements',function() {
+    deepEqual(map(square,[2,3,4]),[4,9,16]);
+  });
+
+  it('should not modify original array',function() {
+    let numbers = [2,3,4];
+    deepEqual(map(square,numbers),[4,9,16]);
+    deepEqual(numbers,[2,3,4]);
   });
 });
 
 describe('filter',function() {
-  it('should return all the array vlaues',function() {
+   it('should return empty array when empty array is given as input',function() {
     deepEqual(filter(returnTrue,[]),[]);
-    deepEqual(filter(returnTrue,[1]),[1]);
-    deepEqual(filter(returnTrue,[1,2]),[1,2]);
-    deepEqual(filter(returnTrue,[1,2,3]),[1,2,3]);
   });
 
-  it('should return even numbers',function() {
-    deepEqual(filter(isEven,[]),[]);
-    deepEqual(filter(isEven,[2]),[2]);
-    deepEqual(filter(isEven,[1,2]),[2]);
-    deepEqual(filter(isEven,[1,2,3,4]),[2,4]);
+  it('should return all elements when filter returns true for all',function() {
+    deepEqual(filter(returnTrue,[3,4,5,2]),[3,4,5,2]);
+  });
+
+  it('should return nothing when filter returns false for all',function() {
+    deepEqual(filter(() => false,[3,4,5,2]),[]);
+  });
+
+  it('should work with more than one elements',function() {
+    deepEqual(filter(x => x%2==0,[2,3,4]),[2,4]);
+  });
+
+  it('should not modify original array',function() {
+    let numbers = [2,3,4];
+    deepEqual(filter(returnTrue,numbers),[2,3,4]);
+    deepEqual(numbers,[2,3,4]);
   });
 });
 
 
 describe('reduce',function() {
-  it('should return sum of array vlaues',function() {
+  it('should work with one element',function() {
     deepEqual(reduce(add,[1]),1);
+  });
+
+  it('should work more than  one element',function() {
     deepEqual(reduce(add,[1,2]),3);
     deepEqual(reduce(add,[1,2,3]),6);
+  });
+
+  it('should support accumulator',function() {
     deepEqual(reduce(add,[1,2,3],10),16);
     deepEqual(reduce(add,[" Puli"],"Tilak"),"Tilak Puli");
+  });
+
+  it('should operate on array from index 0 ',function() {
     deepEqual(reduce(add,[" Puli"," keerthy"],"Tilak"),"Tilak Puli keerthy");
   });
 
